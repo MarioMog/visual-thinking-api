5e40e8e132d1901d27ac2406fc4cdda0d7969779
MarioMog <mamg3108@gmail.com>
2022-04-29 00:50:53 -0500

    test: StudentController ...HaveCertification

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -1,8 +1,15 @@
 const StudentController = require("../../lib/controllers/StudentController");
+const Reader = require("../../lib/utils/Reader");
 
 describe("Suit test for StudentController", () => {
     test("1) Getting all students", () => {
         const res = StudentController.getAllStudents();
         expect(res.length).toBe(51);
+    });
+    test("2) Getting students that have certificacion", () => {
+        const studentsArray = Reader.readJsonFile("./test/services/visualpartnersCopy.json");
+        
+        const res = StudentController.getStudentsHaveCertification(studentsArray);
+        expect(res.length).toBe(1);
     });
 });
