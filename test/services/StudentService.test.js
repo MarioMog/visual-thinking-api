63a17ae9d4eff6f202dc97f315dcc2bb95bf1fa6
MarioMog <mamg3108@gmail.com>
2022-04-29 01:07:15 -0500

    chore: linter

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -5,37 +5,37 @@ describe("Suit test for StudentService", () => {
     test("1.1) Getting students that have certification invalid typeof", () => {
         const studentsArray = 1;
         
-        const res = StudentService.filterStudentsByCertification(studentsArray)
+        const res = StudentService.filterStudentsByCertification(studentsArray);
         expect(res).toMatch(/students should be an array/);
     });
     test("1.2) Getting students that have certification empty array", () => {
         const studentsArray = [];
         
-        const res = StudentService.filterStudentsByCertification(studentsArray)
+        const res = StudentService.filterStudentsByCertification(studentsArray);
         expect(res).toMatch(/students cannot be an empty array/);
     });
     test("1.3) Getting students that have certification", () => {
         const studentsArray = Reader.readJsonFile("./test/services/visualpartnersCopy.json");
         
-        const res = StudentService.filterStudentsByCertification(studentsArray)
+        const res = StudentService.filterStudentsByCertification(studentsArray);
         expect(res.length).toBe(1);
     });
     test("2.1) Getting students that have over 500cr invalid typeof", () => {
         const studentsArray = 1;
         
-        const res = StudentService.filterByCreditsOver500(studentsArray)
+        const res = StudentService.filterByCreditsOver500(studentsArray);
         expect(res).toMatch(/students should be an array/);
     });
     test("2.2) Getting students that have over 500cr empty array", () => {
         const studentsArray = [];
         
-        const res = StudentService.filterByCreditsOver500(studentsArray)
+        const res = StudentService.filterByCreditsOver500(studentsArray);
         expect(res).toMatch(/students cannot be an empty array/);
     });
     test("2.3) Getting students that have over 500cr", () => {
         const studentsArray = Reader.readJsonFile("./test/services/visualpartnersCopy.json");
         
-        const res = StudentService.filterByCreditsOver500(studentsArray)
+        const res = StudentService.filterByCreditsOver500(studentsArray);
         expect(res.length).toBe(2);
     });
 });
