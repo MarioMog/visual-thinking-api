72ca633974354670b348bc0f2efb4043baf021bd
MarioMog <mamg3108@gmail.com>
2022-04-29 01:01:36 -0500

    test: StudentController ...CreditsOver500

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -7,9 +7,15 @@ describe("Suit test for StudentController", () => {
         expect(res.length).toBe(51);
     });
     test("2) Getting students that have certificacion", () => {
-        const studentsArray = Reader.readJsonFile("./test/services/visualpartnersCopy.json");
+        const studentsArray = Reader.readJsonFile("./test/controllers/visualpartnersCopy.json");
         
         const res = StudentController.getStudentsHaveCertification(studentsArray);
         expect(res.length).toBe(1);
+    });
+    test("3) Getting students that have over 500cr empty array", () => {
+        const studentsArray = Reader.readJsonFile("./test/controllers/visualpartnersCopy.json");
+        
+        const res = StudentController.getStudentsHaveCreditsOver500(studentsArray);
+        expect(res.length).toBe(2);
     });
 });
